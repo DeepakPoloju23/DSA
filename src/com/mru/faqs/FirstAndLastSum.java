8c9b1a77756465866f119a7e3309ab0152e58891
DeepakPoloju23 <2311cs010560@mallareddyuniversity.ac.in>
2025-01-23 11:46:34 +0530

    DSA1

---
+++ src/com/mru/faqs/FirstAndLastSum.java
@@ -0,0 +1,9 @@
+package com.mru.faqs;
+
+public class FirstAndLastSum {
+
+	public static void main(String[] args) {
+		new_arr[i]=arr1[i]+arr1[arr1.length-1-i]
+	}
+
+}
---
+++ src/com/mru/faqs/MergeArrays.java
@@ -0,0 +1,20 @@
+package com.mru.faqs;
+import java.util.Arrays;
+public class MergeArrays {
+
+	public static void main(String[] args) {
+		int [] arr1 = {1,2,3};
+		int [] arr2 = {4,5,6};
+		int [] arr3 = new int[arr1.length+arr2.length];
+		for(int i=0;i<arr3.length;i++) {
+			if(i<arr1.length) {
+				arr3[i]=arr1[i];
+			}
+			else {
+				arr3[i]=arr2[i-arr1.length];
+			}
+		}
+System.out.println(Arrays.toString(arr3));
+	}
+
+}
